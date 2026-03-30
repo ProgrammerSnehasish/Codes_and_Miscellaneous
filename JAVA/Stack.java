dc244310b81cc745b6785b1e6897e511a34ac220
ProgrammerSnehasish <programmer.snehasishd900@gmail.com>
2026-03-31 00:18:17 +0530

    New

--- JAVA/Stack.java
+++ JAVA/Stack.java
@@ -1,37 +1,68 @@
-class Stack<T> {
-    private Node<T> top;
-    Stack() {
-        top = null;
+import java.io.*;
+import java.util.Scanner;
+
+ public class Stack {
+    private int capacity;
+    private int top;
+    private int[] arr;
+
+    Stack(int size) {
+        capacity = size;
+        arr = new int[capacity];
+        top = -1;
     }
-    void push(T value) {
-        Node<T> newNode = new Node<>(value);
-        newNode.setNext(top);
-        top = newNode;
-    }
-    Node<T> pop() {
-        if (top == null) {
-            return null;
+
+    public void push(int value) {
+        if(top == capacity-1) {
+            System.out.println("The Stack is overflowed, can't enter data.");
+            return;
         }
-        Node<T> temp = top;
-        top = top.getNext();
-        return temp;
+        arr[++top] = value;
+        System.out.println(value+" pushed into Stack");
     }
-    Node<T> peek() {
-        return top;
-    }
-    boolean isEmpty() {
-        return top == null;
-    }
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        Node<T> current = top;
-        while (current != null) {
-            sb.append(current.getData());
-            if (current.getNext() != null) {
-                sb.append("->");
-            }
-            current = current.getNext();
+
+    public int pop() {
+        if(top == -1) {
+            System.out.println("The Stack is in Underflow condition, can't delete data.");
+            return -1;
         }
-        return sb.toString();
+        return arr[top--];
     }
-}
+
+    public void peek() {
+        if(top == -1) {
+            System.out.println("The stack is empty.");
+            return;
+        }
+        System.out.println("The peek element of the stack is: "+arr[top]);
+    }
+
+    public void display() {
+        if(top ==-1) {
+            System.out.println("There is no data in the stack.");
+            return;
+        }
+        System.out.println("The stack is:");
+        for(int i=top;i>=0;i--){
+            System.out.print(arr[i]);
+        }
+        System.out.print("\n");
+    }
+//  }
+
+//  class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Max capacity of Stack:");
+        int size = scan.nextInt();
+        Stack s = new Stack(size);
+
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.pop();
+        s.peek();
+        s.display();
+    }
+ }
---
+++ JAVA/Stack_old.java
@@ -0,0 +1,37 @@
+// class Stack<T> {
+//     private Node<T> top;
+//     Stack() {
+//         top = null;
+//     }
+//     void push(T value) {
+//         Node<T> newNode = new Node<>(value);
+//         newNode.setNext(top);
+//         top = newNode;
+//     }
+//     Node<T> pop() {
+//         if (top == null) {
+//             return null;
+//         }
+//         Node<T> temp = top;
+//         top = top.getNext();
+//         return temp;
+//     }
+//     Node<T> peek() {
+//         return top;
+//     }
+//     boolean isEmpty() {
+//         return top == null;
+//     }
+//     public String toString() {
+//         StringBuilder sb = new StringBuilder();
+//         Node<T> current = top;
+//         while (current != null) {
+//             sb.append(current.getData());
+//             if (current.getNext() != null) {
+//                 sb.append("->");
+//             }
+//             current = current.getNext();
+//         }
+//         return sb.toString();
+//     }
+// }
